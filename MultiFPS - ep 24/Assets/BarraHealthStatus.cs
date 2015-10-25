3d7880a0f141a70665ca34457280f2e9afdd49f4
EduardoSerdeiro <eduardoassislima@hotmail.com>
2015-10-25 19:03:42 -0200

    ProjetoMultiplayer
    Projeto para estudo do Photon

---
+++ MultiFPS - ep 24/Assets/BarraHealthStatus.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class BarraHealthStatus : MonoBehaviour {
+
+[SerializeField] private Slider slider;
+    private Health vida;
+    private float valorVida;
+	// Use this for initialization
+	void Start () {
+        
+       slider.enabled = false;
+       
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+            slider.enabled = true;
+            // vida = PhotonView.Get(GetComponent<Health>());
+            //vida = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>().currentHitPoints;
+            //slider.value = vida/100;
+            Debug.Log(slider.value);
+        }
+
+	}
+}
---
+++ MultiFPS - ep 24/Assets/Health.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+	public float hitPoints = 100f;
+	public float currentHitPoints;
+
+  // [SerializeField] private NetworkManager networkManager;
+
+
+	// Use this for initialization
+	void Start () {
+		currentHitPoints = hitPoints;
+      //  networkManager = new NetworkManager();
+	}
+
+	[RPC]
+	public void TakeDamage(float amt) {
+		currentHitPoints -= amt;
+
+		if(currentHitPoints <= 0) {
+			Die();
+		}
+	}
+
+    void Update()
+    {
+        currentHitPoints = currentHitPoints;
+        KillforTall();
+    }
+
+    void KillforTall()
+    {
+        if (this.transform.position.y < -30)
+        {
+            NetworkManager nm = GameObject.FindObjectOfType<NetworkManager>();
+            nm.AddChatMessage(PhotonNetwork.player.name + " se matou!");
+           // networkManager.AddChatMessage(PhotonNetwork.player.name + " se matou");
+            Die();
+            
+        }
+    }
+	/*void OnGUI() {
+		if( GetComponent<PhotonView>().isMine && gameObject.tag == "Player" ) {
+			if( GUI.Button(new Rect (Screen.width-100, 0, 100, 40), "Suicide!") ) {
+				Die ();
+			}
+		}
+	}*/
+
+	 void Die() {
+		if( GetComponent<PhotonView>().instantiationId==0 ) {
+			Destroy(gameObject);
+		}
+		else {
+			if( GetComponent<PhotonView>().isMine ) {
+				if( gameObject.tag == "Player" ) {		// This is my actual PLAYER object, then initiate the respawn process
+					NetworkManager nm = GameObject.FindObjectOfType<NetworkManager>();
+                    nm.AddChatMessage(PhotonNetwork.player.name + " foi morto!");
+					nm.standbyCamera.SetActive(true);
+					nm.respawnTimer = 3f;
+				}
+				else if( gameObject.tag == "Bot" ) {
+					Debug.LogError("WARNING: No bot respawn code exists!");
+				}
+
+				PhotonNetwork.Destroy(gameObject);
+			}
+		}
+	}
+}
